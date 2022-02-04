ba525d106b9a338b4180908fc69955a0dc0c5a34
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-04 18:18:47 +0530

    Welcome message

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the stack and queue programs");
+            Console.ReadLine();
+        }
+    }
+}
